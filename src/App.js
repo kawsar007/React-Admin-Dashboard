4b70cb2ef1b56a442e21a5b2d917c47cb32a2cdf
Kawsar <imkawsar007@gmail.com>
2021-06-06 15:27:04 +0600

    create side menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Sidebar from './Components/sidebar/Sidebar';
 import Topbar from './Components/topbar/Topbar';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Topbar/> 
+      <Topbar/>
+      <div className="container">
+         <Sidebar/>
+         <div className="other">
+           Other Page
+          </div>
+      </div>
     </div>
   );
 }
